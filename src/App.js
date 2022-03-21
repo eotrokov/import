407642f4628da0c5a113c5809047cb3028b2856d
Evgeniy Otrokov <e.otrokov@corp.vk.com>
2022-03-21 23:49:43 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+// import connect from '@vkontakte/vkui-connect';
+import { View } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import bridge from '@vkontakte/vk-bridge';
+import Home from './panels/Home';
+import Persik from './panels/Persik';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activePanel: 'home',
+			fetchedUser: null,
+		};
+	}
+
+	componentDidMount() {
+		bridge.subscribe((e) => {
+			switch (e.detail.type) {
+				case 'VKWebAppGetUserInfoResult':
+					this.setState({ fetchedUser: e.detail.data });
+					break;
+				default:
+					console.log(e.detail.type);
+					console.log(e.detail.data);
+			}
+		});
+		bridge.send('VKWebAppGetUserInfo', {});
+	}
+
+	go = (e) => {
+		this.setState({ activePanel: e.currentTarget.dataset.to })
+	};
+
+	render() {
+		return (
+			<View activePanel={this.state.activePanel}>
+				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
+				<Persik id="persik" go={this.go} />
+			</View>
+		);
+	}
+}
+
+export default App;
